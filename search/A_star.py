fb1e2fc911e27826408b5756023d8d2f06d30307
KYUNG MO KWEON <kkweon@gmail.com>
2016-12-14 21:49:32 -0800

    A* Search Algorithm

---
+++ search/A_star.py
@@ -0,0 +1,88 @@
+# ====================
+# A* Search Algorithm
+# ====================
+# Wikipedia: https://en.wikipedia.org/wiki/A*_search_algorithm
+# There exists a heuristic function to give one path a favor over other paths
+# heuristifc function is h(x, y) <= distance to goal
+# from (x, y)
+
+# Example Grid
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+
+# Example Heuristic Function
+# It doesn't have to be accurate
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
+
+init = [0, 0]  # initial point
+goal = [len(grid) - 1, len(grid[0]) - 1]  # goal
+cost = 1  # how much cost for one step
+
+delta = [[-1, 0],  # go up
+         [0, -1],  # go left
+         [1, 0],  # go down
+         [0, 1]]  # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+
+def search(grid, init, goal, cost, heuristic):
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False  # flag set if we can't find expand
+    count = 0
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return "Fail"
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            expand[x][y] = count
+            count += 1
+
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                open = []
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost + heuristic[x2][y2] # Add heuristic value
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+                            action[x2][y2] = g2
+
+    return expand, action
+
+A, B = search(grid, init, goal, cost, heuristic)
+
+from pprint import pprint
+pprint(A)
+print ""
+pprint(B)
