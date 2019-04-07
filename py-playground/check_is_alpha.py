e8ecc2bedd7da22780ad64a6fc46561ca2a8c2ce
Kyung Mo Kweon <kkweon@gmail.com>
2019-04-07 14:46:12 -0700

    Check is alpha

---
+++ py-playground/check_is_alpha.py
@@ -0,0 +1,17 @@
+def is_alpha(x: str) -> bool:
+    """
+    >>> is_alpha("")
+    False
+
+    >>> is_alpha("Abc")
+    True
+
+    >>> is_alpha("ABC")
+    True
+
+    >>> is_alpha("AB1C")
+    False
+    """
+    # return x.isalpha()
+    if not x: return False
+    return all('a' <= c <= 'z' or 'A' <= c <= 'Z' for c in x)
