c98cd41b3dad23e0f3fa24b118bd0461afadd709
Kyung Mo Kweon <kkweon@gmail.com>
2019-04-08 11:16:00 -0700

    Add reservoir sampling

---
+++ py-playground/reservoir_sampling.py
@@ -0,0 +1,23 @@
+from typing import *
+
+import random
+
+def choose(m: int, xs: List[int]) -> List[int]:
+    """
+    Randomly choose m out of xs
+    """
+    assert m <= len(xs)
+
+    ret = xs[:m]
+
+    for i in range(m, len(xs)):
+        idx = random.randint(0, i) # inclusive
+
+        if idx < m:
+            ret[idx] = xs[i]
+
+    return ret
+
+if __name__ == "__main__":
+    xs = [1,2,3,4,5]
+    print(choose(3, xs))
