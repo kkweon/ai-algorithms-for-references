37edb3b5b7f724c0c6083d6d28a94a34def7e2b7
Kyung Mo Kweon <kkweon@gmail.com>
2019-04-14 17:48:52 -0700

    Add find median

--- py-playground/find_kth_lowest_in_two_arrays.py
+++ py-playground/find_kth_lowest_in_two_arrays.py
@@ -36,6 +36,48 @@ def find_kth_lowest_in_2_arrays(xs: List[int], ys: List[int], k: int) -> int:
     return kth(xs, len(xs), ys, len(ys), k)
 
 
+def find_median_in_2_arrays(xs: List[int], ys: List[int]) -> float:
+    """
+    >>> find_median_in_2_arrays([1,3],[2]) # 1 2 3
+    2.0
+    >>> find_median_in_2_arrays([1,2,3],[4,5,6]) # 1 2 3 4 5 6
+    3.5
+    >>> find_median_in_2_arrays([1,2,2],[4,6]) # 1 2 2 4 6
+    2.0
+    >>> find_median_in_2_arrays([1,2,2],[]) # 1 2 2
+    2.0
+    >>> find_median_in_2_arrays([2,2],[]) # 2 2
+    2.0
+    >>> find_median_in_2_arrays([-10,5],[-5,3]) # -10 -5 3 5
+    -1.0
+    """
+    if len(xs) < len(ys):
+        xs, ys = ys, xs
+    if not ys:
+        return 0.5 * (xs[(len(xs)) // 2] + xs[(len(xs) - 1) // 2])
+
+    l, r = 0, len(ys) * 2 - 1
+
+    while l <= r:
+        cut2 = (l + r) >> 1
+        cut1 = len(xs) + len(ys) - cut2
+
+        L1 = xs[(cut1 - 1) // 2] if 0 < cut1 else float("-inf")
+        L2 = ys[(cut2 - 1) // 2] if 0 < cut2 else float("-inf")
+
+        R1 = xs[cut1 // 2] if cut1 < 2 * len(xs) else float("inf")
+        R2 = ys[cut2 // 2] if cut2 < 2 * len(ys) else float("inf")
+
+        if L1 > R2:
+            l = cut2 + 1
+        elif L2 > R1:
+            r = cut2 - 1
+        else:
+            return 0.5 * (max(L1, L2) + min(R1, R2))
+
+    return -1
+
+
 if __name__ == "__main__":
     import doctest
 
