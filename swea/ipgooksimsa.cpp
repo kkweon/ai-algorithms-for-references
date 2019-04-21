0764fb6185be6c9e47dd41433362892eb5bff6c5
Kyung Mo Kweon <kkweon@gmail.com>
2019-04-20 18:54:54 -0700

    SWEA

---
+++ swea/ipgooksimsa.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int T;
+  cin >> T;
+
+  while (T--> 0) {
+
+    // N - kiosk
+    // M - people
+    int N, M;
+    cin >> N >> M;
+
+    vector<int> time(N);
+    for (int i = 0; i < N; ++i) {
+      cin >> time[i];
+    }
+
+    int p = 0;
+    int t = 0;
+
+    while (p < M) {
+    }
+
+
+  }
+  return 0;
+}
