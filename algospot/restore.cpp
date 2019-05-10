deecc88a1bbc54834515128f280e155b21c0c1d5
Kyung Mo Kweon <kkweon@gmail.com>
2019-05-09 22:14:16 -0700

    Add restore

--- algospot/restore.cpp
+++ algospot/restore.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 
 int k;
-string words[15];
+constexpr int MAXN = 15;
+string words[MAXN];
 
-string merge(const string& s1, const string& s2) {
+int cache[MAXN][1<<MAXN];
+int overlap[MAXN][MAXN];
+
+int restore(int last, int used) {
+  if (used == (1 << k) - 1) return 0;
+  int& ret = cache[last][used];
+  if (ret != -1) return ret;
+  ret = 0;
+  for (int next = 0; next < k; ++next) {
+    if ((used & (1 << next)) == 0) {
+      int cand = overlap[last][next] + restore(next, used | 1 << next);
+      ret = max(ret, cand);
+    }
+  }
+  return ret;
+}
+
+int get_overlap(const string& s1, const string& s2) {
   int i = s1.size() - 1;
   int j = 0;
 
@@ -13,15 +31,27 @@ string merge(const string& s1, const string& s2) {
     ++j;
   }
 
-  return s1 + s2.substr(j);
+  return j - (++i);
 }
 
-string find() {
-  string temp = words[0];
-  for (int i = 1; i < k; ++i) {
-    temp = merge(temp, words[i]);
+void init() {
+  memset(cache, -1, sizeof(cache));
+  for (int i = 0; i < k; ++i) {
+    for (int j = 0; j < k; ++j) {
+      overlap[i][j] = get_overlap(words[i], words[j]);
+    }
   }
-  return temp;
+}
+
+string reconstruct(int last, int used) {
+  if (used == (1 << k) - 1) return "";
+  for (int i = 0; i < k; ++i) {
+    if (used & (1 << i)) continue;
+    int if_used = restore(i, used | (1 << i));
+    if (restore(last, used) == if_used)
+      return words[i].substr(overlap[last][i]) + reconstruct(i, used | (1 << i));
+  }
+  return "";
 }
 
 int main(int argc, char* argv[]) {
@@ -32,7 +62,8 @@ int main(int argc, char* argv[]) {
   while(C--) {
     cin >> k;
     for (int i = 0; i < k; ++i) cin >> words[i];
-    cout << find() << '\n';
+    init();
+    cout << reconstruct(0, 0) << '\n';
   }
   return 0;
 }
